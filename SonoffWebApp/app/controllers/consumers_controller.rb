530946b6370dc2c54480abfa4850d48c8926cbe7
Oleksii Kozhyn <suburbakskin@gmail.com>
2018-04-11 23:21:54 +0300

    Created basic actions and views for the Consumer controller

--- SonoffWebApp/app/controllers/consumers_controller.rb
+++ SonoffWebApp/app/controllers/consumers_controller.rb
@@ -1,2 +1,53 @@
 class ConsumersController < ApplicationController
+
+	before_action :set_consumer, only: [:show, :edit, :update, :destroy]
+
+	def index
+		@consumers = Consumer.all.order(:name)
+	end
+
+	def new
+		@consumer = Consumer.new
+	end
+
+	def create
+		@consumer = Consumer.new(consumer_params)
+		if @consumer.save
+			redirect_to consumers_path
+		else
+			render :new
+		end
+	end
+
+	def show
+	end
+
+	def edit
+	end
+
+	def update
+		if @consumer.update(consumer_params)
+			redirect_to @consumer
+		else
+			render :edit
+		end
+	end
+
+	def destroy
+		if @consumer.destroy
+			redirect_to consumers_path
+		else
+			redirect_to :back
+		end
+	end
+
+	private
+
+	def set_consumer
+      @consumer = Consumer.find(params[:id])
+    end
+
+    def consumer_params
+      params.require(:consumer).permit(:name, :phone)
+    end
 end
