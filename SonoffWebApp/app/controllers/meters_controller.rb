e869e6486c14a85dbc07d818b5b10d3fb24ecc34
Oleksii Kozhyn <suburbakskin@gmail.com>
2018-04-12 16:53:24 +0300

    Implemented create and delete the meter

--- SonoffWebApp/app/controllers/meters_controller.rb
+++ SonoffWebApp/app/controllers/meters_controller.rb
@@ -1,2 +1,36 @@
 class MetersController < ApplicationController
+
+  before_action :set_meter, only: [:destroy]
+  before_action :set_consumer, only: [:create, :destroy]
+
+  def create
+    @meter = @consumer.meters.new(meter_params)
+    if @meter.save
+      redirect_to @consumer
+    else
+      render '/consumer/show'
+    end
+  end
+
+  def destroy
+    if @meter.destroy
+      redirect_to @consumer
+    else
+      redirect_to :back
+    end
+  end
+
+  private
+
+  def set_meter
+    @meter = Meter.find(params[:id])
+  end
+
+  def set_consumer
+    @consumer = Consumer.find(params[:consumer_id])
+  end
+
+  def meter_params
+    params.require(:meter).permit(:name, :k_trans, :alarm_value, :alarm_on)
+  end
 end
--- SonoffWebApp/app/models/meter.rb
+++ SonoffWebApp/app/models/meter.rb
@@ -1,3 +1,10 @@
 class Meter < ApplicationRecord
+  before_create :generate_topic
   has_many :energies
+
+  private
+
+  def generate_topic
+    self.topic = "#{self.consumer_id}-#{SecureRandom.uuid}"
+  end
 end
