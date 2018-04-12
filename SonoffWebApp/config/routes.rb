455d2da7c4f8a68a146c438743ed485dcadd3f29
Oleksii Kozhyn <suburbakskin@gmail.com>
2018-04-12 13:09:50 +0300

    Created Meters controller

--- SonoffWebApp/config/routes.rb
+++ SonoffWebApp/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :consumers
+  resources :consumers do
+  	resources :meters
+  end
   
   root 'consumers#index'
 end
