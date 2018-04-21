417cbfb0db431e1bc5fe7d3e9a26b9e988a1c98d
Oleksii Kozhyn <suburbakskin@gmail.com>
2018-04-21 23:02:49 +0300

    Showing energy consumption graphs by selected time and prepared it's for bootstrap

--- SonoffWebApp/app/controllers/meters_controller.rb
+++ SonoffWebApp/app/controllers/meters_controller.rb
@@ -4,8 +4,15 @@ class MetersController < ApplicationController
   before_action :set_consumer, only: [:create, :destroy, :edit, :update]
 
   def show
+    params[:period] ||= 'l24h'
     @energies = @meter.energies.last(10)
-    @chart_data = Energy.select_data_for_chart("meter_id = #{@meter.id} AND time > '#{Time.now - (24 * 60 * 60)}'")
+    if params[:period] == 'l24h'
+      @chart_data = Energy.select_data_for_chart("meter_id = #{@meter.id} AND time > '#{Time.now - (24 * 60 * 60)}'")
+    elsif params[:period] == 'given'
+      @chart_data = Energy.select_data_for_chart("meter_id = #{@meter.id} AND time > '#{convert_time(params[:startTime])}'
+                                                  AND time < '#{convert_time(params[:endTime])}'")
+    end
+    @chart_header = set_chart_header
   end
 
   def create
@@ -44,6 +51,16 @@ class MetersController < ApplicationController
 
   def set_consumer
     @consumer = Consumer.find(params[:consumer_id])
+  end
+
+  # установка заголовка таблицы
+  def set_chart_header
+    params[:period] == 'l24h' ? 'in the last 24 hours' : "from #{params[:startTime]} to #{params[:endTime]}"
+  end
+
+  # парсинг текстового представления даты/времени и создание из него объекта DateTime
+  def convert_time(time)
+    Time.strptime(time, '%d.%m.%Y %H:%M')
   end
 
   def meter_params
