8c41138c86cb791ab6a077ec8ee6987fc313b831
Oleksii Kozhyn <suburbakskin@gmail.com>
2018-05-04 15:59:39 +0300

    Adder showing alert and notice

--- SonoffWebApp/config/application.rb
+++ SonoffWebApp/config/application.rb
@@ -14,5 +14,15 @@ module SonoffWebApp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.action_view.field_error_proc = Proc.new { |html_tag, instance| 
+      class_attr_index = html_tag.index 'class="'
+
+      if class_attr_index
+        html_tag.insert class_attr_index + 7, 'is-invalid '
+      else
+        html_tag.insert html_tag.index('>'), ' class="is-invalid"'
+      end   
+    }
   end
 end
